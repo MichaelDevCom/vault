f2e9de6423ae96be9266ec9831a045b3e8905ad6
Michael <geniusmike1979@gmail.com>
2020-08-21 18:54:10 +0100

    Implement the Keyboard

--- snowman/src/components/Keyboard.js
+++ snowman/src/components/Keyboard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Button from './Button';
 
-const StatusDisplay = () => {
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+const Keyboard = ({onLetterClick, onGetLetterClassName}) => {
+
+  const handleLetterClick = (letter) => onLetterClick && onLetterClick(letter);
+
+  const getLetterClassName = (letter) => onGetLetterClassName ? onGetLetterClassName(letter) : "";
+
+  const lettersMap = letters.map((letter) => {
+    return (
+      <Button key={letter} className={getLetterClassName(letter)} onClick={() => handleLetterClick(letter)}>
+        {letter}
+      </Button>
+    );
+  });
 
   return (
-    <div>StatusDisplay</div>
+    <div className="keyboard">
+      {lettersMap}
+    </div>
   );
 }
 
-export default StatusDisplay;
+export default Keyboard;
