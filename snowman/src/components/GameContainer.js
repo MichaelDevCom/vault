48868d669d35d9331f142d54a6588a93823b9777
Michael <geniusmike1979@gmail.com>
2020-08-21 19:33:13 +0100

    Add guessesState and make Keyboard interactive

--- snowman/src/components/GameContainer.js
+++ snowman/src/components/GameContainer.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import GameInputs from './GameInputs';
 import GameOutputs from './GameOutputs';
 
-const GameContainer = () =>{
+const phrase = "Hello World";
+const initialGuessesState = [];
+
+const GameContainer = () => {
+  const [guessesState, setGuessesState] = useState(initialGuessesState);
+
+  const handleCheckLetter = (letter) => {
+    const upperLetter = letter.toUpperCase();
+    
+    if (!guessesState.some(x => x.letter === upperLetter)){
+      setGuessesState([...guessesState,
+        {
+          letter: upperLetter,
+          status: phrase.toUpperCase().includes(upperLetter) ?
+            'correct' : 
+            'wrong'
+        }
+      ]);
+    }
+  }
 
   return (
     <div className="game-container">
       <GameOutputs />
-      <GameInputs />
+      <GameInputs guesses={guessesState} onCheckLetter={handleCheckLetter} />
     </div>
   );
 }
--- snowman/src/components/GameInputs.js
+++ snowman/src/components/GameInputs.js
@@ -2,11 +2,26 @@ import React from 'react';
 import Keyboard from './Keyboard';
 import Button from './Button';
 
-const GameInputs = () => {
+const GameInputs = ({guesses, onCheckLetter}) => {
+
+  const handleGetLetterClassName = (letter) => {
+    const status = guesses && guesses.find(x => x.letter === letter)?.status;
+    if (status) {
+      return status === 'correct' ? 'back-green' : 'back-red';
+    }
+
+    return '';
+  }
+
+  const handleLetterClick = (letter) => {
+    if (guesses && !guesses.some(x => x.letter === letter)){
+      onCheckLetter && onCheckLetter(letter);
+    }
+  }
 
   return (
     <div>
-      <Keyboard />
+      <Keyboard onGetLetterClassName={handleGetLetterClassName} onLetterClick={handleLetterClick} />
       <Button>New game</Button>
     </div>
   );
