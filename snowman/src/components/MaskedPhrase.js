75ec7b4455224466e2aca73f369e7d64c23c366a
Michael <geniusmike1979@gmail.com>
2020-08-25 17:26:57 +0100

    Show the phrase when the game is lost

--- snowman/src/components/MaskedPhrase.js
+++ snowman/src/components/MaskedPhrase.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 const MaskedPhrase = ({gameState, phrase, guesses}) => {
-
-  const output = phrase && guesses && phrase.split('').map((letter) => {
-    if (letter.toLowerCase() !== letter.toUpperCase()) {
-      return guesses.some(x => x.letter === letter.toUpperCase()) ?
-        letter :
-        '_';
-    }
-    else {
-      return letter;
-    }
-  });
+  let output;
+  
+  if (gameState === 'lose'){
+    output = phrase;
+  } else {
+    output = phrase && guesses && phrase.split('').map((letter) => {
+      if (letter.toLowerCase() !== letter.toUpperCase()) {
+        return guesses.some(x => x.letter === letter.toUpperCase()) ?
+          letter :
+          '_';
+      }
+      else {
+        return letter;
+      }
+    });
+  }
 
   let classes = 'masked-phrase';
     if (phrase.length > 20) {
